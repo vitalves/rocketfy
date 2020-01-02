c98eaead7b09b5d975e1e3163f3f9a4b3a54a894
vitalves <vitalvesilva@gmail.com>
2020-01-02 13:52:52 -0300

    config init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
@@ -12,21 +9,6 @@ function App() {
         <title>Página inicial</title>
         <meta name="description" content="Um teste com Helmet" />
       </Helmet>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
